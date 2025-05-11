039aa973b8178ea202ad39590f187c0a6a2a5e5d
Joodith <joodithl@gmail.com>
2025-05-11 20:01:22 +0530

    chore: Add Terraform validation and security checks

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -34,3 +34,39 @@ variable "enable_secret_rotation" {
   type        = bool
   default     = false
 }
+
+variable "aws_account_id" {
+  description = "AWS Account ID for resource creation - must be provided via environment variable TF_VAR_aws_account_id"
+  type        = string
+  sensitive   = true  # Marks this variable as sensitive in logs and outputs
+}
+
+variable "tf_state_bucket" {
+  description = "Name of the S3 bucket for Terraform state"
+  type        = string
+  default     = "resume-builder-tf-state"
+}
+
+variable "app_name" {
+  description = "Name of the application, used in resource naming"
+  type        = string
+  default     = "resume-builder"
+}
+
+variable "ecr_repositories" {
+  description = "Names of ECR repositories to be created"
+  type        = list(string)
+  default     = ["resume-backend", "resume-frontend"]
+}
+
+variable "oidc_provider_url" {
+  description = "GitHub OIDC provider URL"
+  type        = string
+  default     = "token.actions.githubusercontent.com"
+}
+
+variable "iam_role_prefix" {
+  description = "Prefix for IAM roles"
+  type        = string
+  default     = "github-actions-oidc-role"
+}
